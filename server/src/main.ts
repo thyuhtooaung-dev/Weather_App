7a7a27e50bbe69704576a3009303c693c1fff58d
ThyuHtooAungOwO <thyuhtoo7@gmail.com>
2026-02-08 19:06:31 +0630

    fix: enable trusted proxy in main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.set('trust proxy', 1);
   app.enableCors({
     origin: [
       'http://localhost:5173',
       'http://127.0.0.1:5173',
       'https://weather-app-pi-eosin-28.vercel.app',
+      configService.getOrThrow('CLIENT_URL'),
     ],
     methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS',
     credentials: true,
